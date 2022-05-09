9d17cd21ae86a8cdc6551f594c95fb928f078463
Yoan Thirion <yoan.thirion@gmail.com>
2022-05-09 16:26:02 +0200

    change extensions

--- c#/ArchUnit-demo-tests/ArchUnitExtensions.cs
+++ c#/ArchUnit-demo-tests/ArchUnitExtensions.cs
@@ -10,13 +10,13 @@ namespace ArchUnit.Demo.Tests
 {
     public static class ArchUnitExtensions
     {
-        public static GivenTypesConjunction TypesInAssembly() =>
-            Types().That().Are(Architecture.Types);
-
         private static readonly Architecture Architecture =
             new ArchLoader()
                 .LoadAssemblies(typeof(SuperHeroController).Assembly)
                 .Build();
+        
+        public static GivenTypesConjunction TypesInAssembly() =>
+            Types().That().Are(Architecture.Types);
 
         public static void Check(this IArchRule rule) => rule.Check(Architecture);
     }
